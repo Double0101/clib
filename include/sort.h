ae68bbe5edc4245998e7bc56cf64b88edfc646a6
Double <double0101@foxmail.com>
2019-04-08 15:22:35 +0800

    mergesort

--- include/sort.h
+++ include/sort.h
@@ -5,5 +5,9 @@ void bubble_sort_arraylist(struct arraylist* list, int start, int end, int compa
 void bubble_sort_linkedlist(struct linkedlist* list, int start, int end, int compare(void* data1, void* data2));
 void quick_sort_arraylist(struct arraylist* list, int start, int end, int compare(void* data1, void* data2));
 void quick_sort_linkedlist(struct linkedlist* list, int start, int end, int compare(void* data1, void* data2));
+void insertion_sort_arraylist(struct arraylist* list, int start, int end, int compare(void* data1, void* data2));
+void insertion_sort_linkedlist(struct linkedlist* list, int start, int end, int compare(void* data1, void* data2));
+void merge_sort_arraylist(struct arraylist* list, int start, int end, int compare(void* data1, void* data2));
+void merge_sort_linkedlist(struct linkedlist* list, int start, int end, int compare(void* data1, void* data2));
 
 #endif
--- util/list/sort.c
+++ util/list/sort.c
@@ -11,6 +11,8 @@
 
 #include "../../include/sort.h"
 
+const char _TAIL_[] = "tail";
+
 void
 bubble_sort_arraylist(struct arraylist* list, int start, int end, int compare(void* data1, void* data2))
 {
@@ -142,5 +144,101 @@ insertion_sort_linkedlist(struct linkedlist* list, int start, int end, int compa
             --j;
         }
         linkedlist_get(list, j + 1) -> data = element -> data;
+    }
+}
+
+void
+arraylist_merge(struct arraylist* list, int start, int divide, int end, int compare(void* data1, void* data2))
+{
+    int n1 = divide - start + 1;
+    int n2 = end - divide;
+    void* l1[n1];
+    void* l2[n2];
+
+    int i, j;
+    for (i = 0; i < n1; ++i)
+        l1[i] = list -> data[start + i];
+    for (j = 0; j < n2; ++j)
+        l2[j] = list -> data[divide + j + 1];
+
+    i = 0, j = 0;
+    for (int k = 0; k < end - start + 1; ++k)
+    {
+        if (i < n1 && j < n2)
+        {
+            if (compare(l1[i], l2[j]) > 0)
+            {
+                list -> data[k] = l2[j++];
+            }
+            else
+            {
+                list -> data[k] = l1[i++];
+            }
+        }
+        else
+        {
+            if (i == n1) list -> data[k] = l2[j++];
+            else list -> data[k] = l1[i++];
+        }
+    }
+}
+
+void
+linkedlist_merge(struct linkedlist* list, int start, int divide, int end, int compare(void* data1, void* data2))
+{
+    int n1 = divide - start + 1;
+    int n2 = end - divide;
+    void *l1[n1];
+    void *l2[n2];
+
+    int i, j;
+    for (i = 0; i < n1; ++i)
+        l1[i] = linkedlist_get(list, start + i);
+    for (j = 0; j < n2; ++j)
+        l2[j] = linkedlist_get(list, divide + j + 1);
+    i = 0, j = 0;
+
+    for (int k = 0; k < end - start + 1; ++k) {
+        if (i < n1 && j < n2)
+        {
+            if (compare(l1[i], l2[j]) > 0)
+            {
+                linkedlist_get(list, k) -> data = l2[j++];
+            }
+            else
+            {
+                linkedlist_get(list, k) -> data = l1[i++];
+            }
+        }
+        else
+        {
+            if (i == n1) linkedlist_get(list, k) -> data = l2[j++];
+            else linkedlist_get(list, k) -> data = l1[i++];
+        }
+    }
+}
+
+void
+merge_sort_arraylist(struct arraylist* list, int start, int end, int compare(void* data1, void* data2))
+{
+    if (end > start)
+    {
+        int div = (start + end) / 2;
+        merge_sort_arraylist(list, start, div, compare);
+        merge_sort_arraylist(list, div + 1, end, compare);
+        arraylist_merge(list, start, div, end, compare);
+    }
+}
+
+
+void
+merge_sort_linkedlist(struct linkedlist* list, int start, int end, int compare(void* data1, void* data2))
+{
+    if (end > start)
+    {
+        int div = (start + end) / 2;
+        merge_sort_linkedlist(list, start, div, compare);
+        merge_sort_linkedlist(list, div + 1, end, compare);
+        linkedlist_merge(list, start, div, end, compare);
     }
 }
